8a964a76e3a6b11ec09c426e907baf0bf6f6592c
dungctk34@gmail.com <dungctk34@gmail.com@9278d715-9f83-dda7-1b8c-591cbdcad238>
2013-12-08 17:52:02 +0000

    sua file TaiKhoan.php, PhanQuyen.php,PhieuDichVu.php. doi ten class AccountController.php ->TaiKhoanController.php va doi ten lop AccountView.php ->TaiKhoanView.php

--- trunk/QuanLyKhachSan/Models/PhanQuyen.php
+++ trunk/QuanLyKhachSan/Models/PhanQuyen.php
@@ -11,7 +11,53 @@
  * @author Admin
  */
 class PhanQuyen {
-    //put your code here
+    
+    var $idPhanQuyen;
+    var $idTaiKhoan;
+    var $Quyen;
+    var $Ngay;
+    
+    function __construct($idPhanQuyen, $idTaiKhoan, $Quyen, $Ngay) {
+        $this->idPhanQuyen = $idPhanQuyen;
+        $this->idTaiKhoan = $idTaiKhoan;
+        $this->Quyen = $Quyen;
+        $this->Ngay = $Ngay;
+    }
+    
+    public function getIdPhanQuyen() {
+        return $this->idPhanQuyen;
+    }
+
+    public function getIdTaiKhoan() {
+        return $this->idTaiKhoan;
+    }
+
+    public function getQuyen() {
+        return $this->Quyen;
+    }
+
+    public function getNgay() {
+        return $this->Ngay;
+    }
+
+    public function setIdPhanQuyen($idPhanQuyen) {
+        $this->idPhanQuyen = $idPhanQuyen;
+    }
+
+    public function setIdTaiKhoan($idTaiKhoan) {
+        $this->idTaiKhoan = $idTaiKhoan;
+    }
+
+    public function setQuyen($Quyen) {
+        $this->Quyen = $Quyen;
+    }
+
+    public function setNgay($Ngay) {
+        $this->Ngay = $Ngay;
+    }
+
+
+
 }
 
 ?>
--- trunk/QuanLyKhachSan/Models/TaiKhoan.php
+++ trunk/QuanLyKhachSan/Models/TaiKhoan.php
@@ -1,9 +1,63 @@
 <?php
 
 
-class Account {
-    public $id = 0;
-    public $name = "";
+class TaiKhoan {
+    var $idTaiKhoan;
+    var $TenTK;
+    var $MatKhau;
+    var $PhanQuyen_idPhanQuyen;
+    var $PhanQuyen_idTaiKhoan;
+    
+    function __construct($idTaiKhoan, $TenTK, $MatKhau, $PhanQuyen_idPhanQuyen, $PhanQuyen_idTaiKhoan) {
+        $this->idTaiKhoan = $idTaiKhoan;
+        $this->TenTK = $TenTK;
+        $this->MatKhau = $MatKhau;
+        $this->PhanQuyen_idPhanQuyen = $PhanQuyen_idPhanQuyen;
+        $this->PhanQuyen_idTaiKhoan = $PhanQuyen_idTaiKhoan;
+    }
+    
+    public function getIdTaiKhoan() {
+        return $this->idTaiKhoan;
+    }
+
+    public function getTenTK() {
+        return $this->TenTK;
+    }
+
+    public function getMatKhau() {
+        return $this->MatKhau;
+    }
+
+    public function getPhanQuyen_idPhanQuyen() {
+        return $this->PhanQuyen_idPhanQuyen;
+    }
+
+    public function getPhanQuyen_idTaiKhoan() {
+        return $this->PhanQuyen_idTaiKhoan;
+    }
+
+    public function setIdTaiKhoan($idTaiKhoan) {
+        $this->idTaiKhoan = $idTaiKhoan;
+    }
+
+    public function setTenTK($TenTK) {
+        $this->TenTK = $TenTK;
+    }
+
+    public function setMatKhau($MatKhau) {
+        $this->MatKhau = $MatKhau;
+    }
+
+    public function setPhanQuyen_idPhanQuyen($PhanQuyen_idPhanQuyen) {
+        $this->PhanQuyen_idPhanQuyen = $PhanQuyen_idPhanQuyen;
+    }
+
+    public function setPhanQuyen_idTaiKhoan($PhanQuyen_idTaiKhoan) {
+        $this->PhanQuyen_idTaiKhoan = $PhanQuyen_idTaiKhoan;
+    }
+
+
+
 }
 
 ?>
