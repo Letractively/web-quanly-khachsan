8be7bef139c4f642002b6f01c49f25c7a57e5cf4
nguyenvu.ctk34@gmail.com <nguyenvu.ctk34@gmail.com@9278d715-9f83-dda7-1b8c-591cbdcad238>
2013-12-10 17:30:21 +0000

    bổ sung, hoàn thiện trang index thêm thư mục hotelimg chứa hình ảnh về khách sạn thêm thư mục icon chứa các file icon thêm các file css và js

--- trunk/include/botpage.php
+++ trunk/include/botpage.php
@@ -5,3 +5,11 @@
  * and open the template in the editor.
  */
 ?>
+<div id="footer-line"></div>
+<footer id="botpage-info">
+    
+    <h2>© 2013 XuanHongHotel.com.vn - Khách sạn Xuân Hồng Đà Lạt</h2>
+    <p>Địa chỉ: 39, Nam Kỳ Khởi Nghĩa, Đà Lạt, Việt Nam - Email: xuanhong@dalathotel.vn</p>
+    <p>Điện thoại: (84-063)3831483 - 0918007164</p>
+    <p>Design by Group09</p>
+</footer>
--- trunk/include/midpage.php
+++ trunk/include/midpage.php
@@ -7,23 +7,98 @@
 ?>
 <div id="midpage-slideimage">
     <div id="midpage-slideimage-big">
-        
+        <img src="hotelimg/7.png">
     </div>
     <div id="midpage-slideimage-thumb">
-        
+        <ul>
+            <li>
+                <img src="hotelimg/thum-1.png">
+            </li>
+            <li>
+                <img src="hotelimg/thum-2.png">
+            </li>
+            <li>
+                <img src="hotelimg/thum-3.png">
+            </li>
+            <li>
+                <img src="hotelimg/thum-4.png">
+            </li>
+            <li>
+                <img src="hotelimg/thum-5.png">
+            </li>
+            <li>
+                <img src="hotelimg/thum-6.png">
+            </li>
+            <li>
+                <img src="hotelimg/thum-7.png">
+            </li>
+        </ul>
     </div>
+</div>
+<div>
+    <img src="image/banner_border_bottom.gif">
 </div>
 <div id="midpage-post">
     <div id="midpage-post-left">
-        <div id="midpage-post-left-calendar"></div>
-        <div id="midpage-post-left-events"></div>
-        <div id="midpage-post-left-contact"></div>
+        <fieldset class="mid-left">
+            <legend>LỊCH</legend>
+        <div id="midpage-post-left-calendar">
+            <script type="text/javascript" language="JavaScript" src="script/amlich.js">
+</script>
+<script language="JavaScript">
+setOutputSize("small");
+document.writeln(printSelectedMonth());
+</script>
+        </div>
+            </fieldset>
+        <img src="image/line.png" CLASS="mid-left-line">
+        
+        <fieldset class="mid-left">
+            <legend>SỰ KIỆN</legend>
+        <div id="midpage-post-left-events">
+            <ul>
+                <li>
+                    <p><a href="#"> Event 1</a></p>
+                </li>
+                <li>
+                    <p><a href="#"> Event 1</a></p>
+                </li>
+                <li>
+                    <p><a href="#"> Event 1</a></p>
+                </li>
+                <li>
+                    <p><a href="#"> Event 1</a></p>
+                </li>
+                <li>
+                    <p><a href="#"> Event 1</a></p>
+                </li>
+                <li>
+                    <p><a href="#"> Event 1</a></p>
+                </li>
+            </ul>
+        </div>
+         </fieldset>
+        <img src="image/line.png" CLASS="mid-left-line">
+       
+        <fieldset class="mid-left">
+            <legend>LIÊN HỆ</legend>
+        <div id="midpage-post-left-contact">
+            <ul>
+                <li>
+                    <p>111</p>
+                </li>
+                <li>
+                    <p>112</p>
+                </li>
+                <li>
+                    <p>113</p>
+                </li>
+            </ul>
+        </div>
+         </fieldset>
+        <img src="image/line.png" CLASS="mid-left-line">
+       
     </div>
     <div id="midpage-post-right">
-        <div id="midpage-post-right-location">
-            <div id="midpage-post-right-location-img"></div>
-            <div id="midpage-post-right-location-map"></div>
-        </div>
-        <div id="midpage-post-right-roomkinds"></div>
     </div>
 </div>
