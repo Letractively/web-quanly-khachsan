c7a74c37508ff83605f296dc98243e6a8167ef23
nguyenvu.ctk34@gmail.com <nguyenvu.ctk34@gmail.com@9278d715-9f83-dda7-1b8c-591cbdcad238>
2013-12-14 16:04:14 +0000

    hoàn ch?nh trang hình ?nh, tin t?c, ph?n h?i

--- trunk/include/botpage.php
+++ trunk/include/botpage.php
@@ -8,7 +8,7 @@
 <div id="footer-line"></div>
 <footer id="botpage-info">
     
-    <h2>© 2013 XuanHongHotel.com.vn - Khách sạn Xuân Hồng Đà Lạt</h2>
+    <h3>© 2013 XuanHongHotel.com.vn - Khách sạn Xuân Hồng Đà Lạt</h3>
     <p>Địa chỉ: 39, Nam Kỳ Khởi Nghĩa, Đà Lạt, Việt Nam - Email: xuanhong@dalathotel.vn</p>
     <p>Điện thoại: (84-063)3831483 - 0918007164</p>
     <p>Phát triển bởi Group09</p>
--- trunk/include/feedback.php
+++ trunk/include/feedback.php
@@ -5,4 +5,37 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+?>
+<div id="feedback">
+    <h2><b>< phản hồi của khách hàng ></b></h2>
+    <div id="feedback-info">
+        <fieldset>
+            <legend>THÔNG TIN</legend>
+            <form >
+                <table style="width: 580px;">
+                    <tr>
+                        <td><label>Họ tên khách hàng : </label></td>
+                        <td><input class="text" type="text" placeholder="Họ tên khách hàng..." name="name"></td>
+                    </tr>
+                    <tr>
+                        <td><label>Email : </label></td>
+                        <td><input class="text" type="email" placeholder="Thư điện tử..." name="name"></td>
+                    </tr>
+                    <tr>
+                        <td><label>Số điện thoại : </label></td>
+                        <td><input class="text" type="tel" placeholder="Số điện thoại..." name="name"></td>
+                    </tr>
+                    <tr>
+                        <td><label>Ý kiến phản hồi : </label></td>
+                        <td><textarea   class="textarea" placeholder="Ý kiến phản hồi..." cols="1" rows="10"></textarea></td>
+                    </tr>
+                </table>
+                <div id="feedback-button">
+                    <input type="reset" name="reset" value="Huỷ" class="btn">
+                    <input type="submit" name="submit" value="Gửi" class="btn">
+                </div>
+            </form>
+        </fieldset>
+    </div>
+</div>
 
