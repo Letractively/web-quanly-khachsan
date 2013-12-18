797be7d73de88e0f50c0f9760b7f74833bd6ee0a
nguyenvu.ctk34 <nguyenvu.ctk34@gmail.com>
2013-12-18 13:32:48 +0000

    hoàn tất trang contact

--- include/contact.php
+++ include/contact.php
@@ -5,4 +5,48 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+?>
+<div id="contactpage">
+    <h2><b>< thông tin liên hệ ></b></h2>
+    <img class="line-image" src="image/banner_border_bottom.gif" alt="line"></br></br>
+    <div class="contactpage-info">
+        <div class="contactpage-info-image">
+            <img src="icons/phone.png" alt="contact">
+        </div>
+        <div class="contactpage-info-detail">
+            <p><b>Nhân viên :</b> Đinh Trí Dũng</p>
+            <p><b>Phone :</b> 1011xxx</p>
+            <p><b>Email :</b> abc01@gmail.com</p>
+        </div>
+    </div>
+    <div class="contactpage-info">
+        <div class="contactpage-info-image">
+            <img src="icons/phone.png" alt="contact">
+        </div>
+        <div class="contactpage-info-detail">
+            <p><b>Nhân viên :</b> Nguyễn Nam Phú</p>
+            <p><b>Phone :</b> 1011xxx</p>
+            <p><b>Email :</b> abc02@gmail.com</p>
+        </div>
+    </div>
+    <div class="contactpage-info">
+        <div class="contactpage-info-image">
+            <img src="icons/phone.png" alt="contact">
+        </div>
+        <div class="contactpage-info-detail">
+            <p><b>Nhân viên :</b> Đặng Nguyễn Nguyên Vũ</p>
+            <p><b>Phone :</b> 1011xxx</p>
+            <p><b>Email :</b> abc03@gmail.com</p>
+        </div>
+    </div>
+    <div class="contactpage-info">
+        <div class="contactpage-info-image">
+            <img src="icons/phone.png" alt="contact">
+        </div>
+        <div class="contactpage-info-detail">
+            <p><b>Nhân viên :</b> Đỗ Ngọc Dân</p>
+            <p><b>Phone :</b> 1011xxx</p>
+            <p><b>Email :</b> abc04@gmail.com</p>
+        </div>
+    </div>
+</div>
