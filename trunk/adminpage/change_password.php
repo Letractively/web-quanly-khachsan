6d6ed420f831473e808ae1d79614c0f91b028793
dungctk34@gmail.com <dungctk34@gmail.com@9278d715-9f83-dda7-1b8c-591cbdcad238>
2013-12-28 16:57:58 +0000

    sua loi doi mat khau file change_password.php

--- trunk/adminpage/change_password.php
+++ trunk/adminpage/change_password.php
@@ -6,17 +6,17 @@
 				<tr>
 					<td colspan="2">
 						<?php 
-							$user_id = $_SESSION['authentication']['user_id'];
+							$idtaikhoan = $_SESSION['authentication']['idtaikhoan'];
 							if(isset($_POST['change'])){
 								$oldpass = $_POST['oldpass'];
 								$newpass1 = $_POST['newpass1'];
 								$newpass2 = $_POST['newpass2'];
 								$cnn = DB::StaticConnect();
-								$rs = DB::ExecuteQuery("select user_name from user where user_id = '$user_id' and password = '".md5($oldpass)."'",$cnn);
+								$rs = DB::ExecuteQuery("select tentk from taikhoan where idtaikhoan = '$idtaikhoan' and matkhau = '".md5($oldpass)."'",$cnn);
 								if(mysql_num_rows($rs) > 0){
 									if($newpass1 != "" && $newpass2 != "" && $newpass1 == $newpass2){
 										$cnn2 = DB::StaticConnect();
-										$rs2 = DB::ExecuteQuery("update user set password = '".md5($newpass1)."' where user_id = '$user_id'", $cnn2);
+										$rs2 = DB::ExecuteQuery("update taikhoan set matkhau = '".md5($newpass1)."' where idtaikhoan = '$idtaikhoan'", $cnn2);
 										if($rs2){
 											echo "<p style='color:blue;'>Đổi mật khẩu thành công.</p>";
 										}else{
