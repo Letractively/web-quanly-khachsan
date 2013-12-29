ca51bdafbb5bb0ddd1130d9b0409bcf364b6123f
nguyenvu.ctk34 <nguyenvu.ctk34@gmail.com>
2013-12-29 03:22:46 +0000

    thêm file CSDL ver6 chỉnh sửa trang thuê phòng

--- include/workpages/spendingform.php
+++ include/workpages/spendingform.php
@@ -1,25 +1,42 @@
 <?php
+//kết nối csdl
+$db = mysql_connect("localhost", "root", "");
+if (!$db) {
+    echo "Không thể kết nối";
+    exit();
+    
+}
+mysql_select_db("quanlykhachsan");
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 ?>
-<div id="spendingform">
-    <div id="spendingform-addcustomer">
-        <?php
-            require_once './Views/KhachHangView.php';
-        ?>
+<div id="bookingform">
+    <div id="bookingform-addcustomer">
+     <?php
+     require_once './Views/KhachHangView.php';
+     ?>
     </div>
-    <div id="spendingform-form">
-        <fieldset id="spendingform-form-info">
+    <div id="bookingform-form">
+        <fieldset id="bookingform-form-info">
        <legend>Thuê phòng</legend>
-            <form>
+       <form action ="index.php?act=profile&w=spending" method="post">
                 <table>
                     <tr>
                         <td><label>Mã khách hàng :</label></td>
-                        <td><input class="text" type="text" name="mkh" placeholder="Mã khách hàng..."></td>
+                        <td> <select name="makh" id="makh" class="cbb">
+                                <option value="">Chọn khách hàng</option>
+                                   <?php
+                                    $sql = "select * from khachhang";
+                                    $result = mysql_query($sql,$db);
+                                    $n = mysql_num_rows($result);
+                                    if (mysql_num_rows($result)<>0) {
+                                          while($row = mysql_fetch_row($result)){
+                                              $makh =$row[0];
+                                               echo "<option value ='$makh'>$makh</option>";
+                                           }
+                                    }
+                                   ?>
+                                </select>
+            </td>
                     </tr>
                     <tr>
                         <td><label>Phòng :</label></td>
@@ -38,20 +55,49 @@
                             </select>
                         </td>
                     </tr>
+                    <!--<tr>
+                        <td><label>Ngày đặt :</label></td>
+                        <td><input class="text" type="date" name="cdate" ></td>
+                    </tr>-->
                     <tr>
-                        <td><label>Ngày đến :</label></td>
+                        <td><label>Ngày nhận :</label></td>
                         <td><input class="text" type="date" name="cdate"></td>
                     </tr>
                     <tr>
                         <td></td>
                         <td>
-                            <input class="btn" type="reset" value="Huỷ" name="reset">
+                            <input class="btn" type="reset" value="Huỷ" name="reset" action="null">
                             <input class="btn" type="submit" value="Thêm" name="submit">
                         </td>
                     </tr>
              </table>
       </form>
+       
+       <?php
+       if (isset($_POST["makh"]) && isset($_POST["room"])) {
+           $makh = $_POST["makh"];
+           $room = $_POST["room"];
+           $ngaydat = $_POST["cdate"];
+           $ngaynhan = $_POST["cdate"];
+           
+           if (!is_numeric($makh) ){
+               echo "Kiểm tra thông tin";
+               exit();
+           }
+           $sql = "INSERT INTO thuephong VALUES
+               ('$makh','$room','$ngaydat','$ngaynhan')";
+          
+           
+           $result = mysql_query($sql,$db);
+          
+           if ($result) {
+               echo "<p align ='center'>Thành Công</p>";
+           }
+       }
+  ?>
+       
 </fieldset>
-        <input type="button" value="In phiếu thuê" class="btn" name="printspend">
+        <a href="Views/printHD.php"><input type="button" value="In phiếu" class="btn" name="prtbooking"></a>
+        
     </div>
 </div>
